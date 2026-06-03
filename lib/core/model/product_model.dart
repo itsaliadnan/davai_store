ec923c3bc853ebf3f8c7bc0d612abdcd0590307a
ali adnan <its.ali.adnan@gmail.com>
2026-06-03 22:31:34 +0300

    product_model

--- lib/core/model/product_model.dart
+++ lib/core/model/product_model.dart
@@ -5,11 +5,13 @@ class Product {
   final String image;
   final double price;
   final List<Color> colors;
+  final Color selectedColor;
 
   const Product({
     required this.title,
     required this.image,
     required this.price,
     this.colors = const [],
+    this.selectedColor = Colors.red,
   });
 }
