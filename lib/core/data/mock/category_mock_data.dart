28bdb911341b4a4bd3d02620181ba94da9b4a598
ali adnan <its.ali.adnan@gmail.com>
2026-09-12 00:43:35 +0300

    deleted files

--- lib/core/data/mock/category_mock_data.dart
+++
@@ -1,7 +0,0 @@
-import 'package:davai_store/core/model/category_model.dart';
-
-final categories = [
-  CategoryModel(title: 'Women'),
-  CategoryModel(title: 'Men'),
-  CategoryModel(title: 'Children'),
-];
